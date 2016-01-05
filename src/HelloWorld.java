539d2c52a22d0191a37820cd2c127df6e9096d5f
William Beckler <wsbeckler@gmail.com>
2016-01-04 21:57:48 -0500

    added addition calculation funtionality

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 /**
  * Created by WilliamBeckler on 1/4/2016.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        int first;
+        int second;
+        String str1 = "0";
+        String str2 = "0";
+
+        System.out.println("Please input a number: ");
+        first = scan.nextInt();
+        System.out.println("You entered the number: " + first);
+        System.out.println("Please input another number: ");
+        second = scan.nextInt();
+        System.out.println("You entered the number: " + second);
+        System.out.println("Your total is: " + sum(first,second));
+    }
+
+    public static int sum(int a, int b){
+        return a+b;
     }
 }
